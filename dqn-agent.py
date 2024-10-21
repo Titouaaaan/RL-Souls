b4214ea9054ea1849a8a5f33b1516ccb926156ec
Titouan <titouanguerin@gmail.com>
2024-10-21 16:23:52 +0200

    updated dqn agent

--- dqn-agent.py
+++ dqn-agent.py
@@ -3,10 +3,13 @@ import soulsgym
 
 import math
 import random
-import matplotlib
-import matplotlib.pyplot as plt
+import numpy
+# import matplotlib
+# import matplotlib.pyplot as plt
 from collections import namedtuple, deque
 from itertools import count
+import time
+import plotly.graph_objects as go
 
 import torch
 import torch.nn as nn
@@ -18,11 +21,11 @@ assert torch.cuda.is_available()
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
 
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
+# is_ipython = 'inline' in matplotlib.get_backend()
+# if is_ipython:
+#     from IPython import display
 
-plt.ion()
+# plt.ion()
 
 device = torch.device(
     "cuda" if torch.cuda.is_available() else
@@ -34,6 +37,7 @@ env = gymnasium.make("SoulsGymIudex-v0")
 
 def preprocess_state(state_dict):
     state_values = []
+    # for debugging atm
     
     # Add phase, player_hp, player_max_hp, player_sp, player_max_sp, boss_hp, boss_max_hp
     state_values.append(state_dict['phase'])
@@ -103,10 +107,10 @@ class DQN(nn.Module):
 # LR is the learning rate of the ``AdamW`` optimizer
 BATCH_SIZE = 128
 GAMMA = 0.99
-EPS_START = 0.9
+EPS_START = 0.95
 EPS_END = 0.05
-EPS_DECAY = 1000
-TAU = 0.005
+EPS_DECAY = 5000
+TAU = 0.01
 LR = 1e-4
 
 # Get number of actions from gym action space
@@ -146,32 +150,6 @@ def select_action(state):
 
 
 episode_durations = []
-
-
-def plot_durations(show_result=False):
-    plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
-    # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
-
-    plt.pause(0.001)  # pause a bit so that plots are updated
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
-        else:
-            display.display(plt.gcf())
 
 def optimize_model():
     if len(memory) < BATCH_SIZE:
@@ -216,7 +194,7 @@ def optimize_model():
     optimizer.zero_grad()
     loss.backward()
     # In-place gradient clipping
-    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100) # change vale here to lower for more stability?
     optimizer.step()
 
 if torch.cuda.is_available() or torch.backends.mps.is_available():
@@ -228,12 +206,17 @@ for i_episode in range(num_episodes):
     # Initialize the environment and get its state
     state, info = env.reset()
     state = preprocess_state(state)
-    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    # state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    state = state.clone().detach().unsqueeze(0)
     for t in count():
         action = select_action(state)
+
+        #maybe add more intermediate rewards here?
         observation, reward, terminated, truncated, _ = env.step(action.item())
+
         observation = preprocess_state(observation)
         reward = torch.tensor([reward], device=device)
+        print(f"reward at time {t}: {reward}")
         done = terminated or truncated
 
         if terminated:
@@ -260,10 +243,6 @@ for i_episode in range(num_episodes):
 
         if done:
             episode_durations.append(t + 1)
-            plot_durations()
             break
 
 print('Complete')
-plot_durations(show_result=True)
-plt.ioff()
-plt.show()
