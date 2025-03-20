c22b176b6e5be7d4bf88e83a5cc49361291febc7
Titouan <titouanguerin@gmail.com>
2025-03-20 13:39:18 +0100

    changes so it works with the new changes

--- DDDQN/eval_agent.py
+++ DDDQN/eval_agent.py
@@ -32,26 +32,24 @@ parser.add_argument('--save_interval', type=int, default=int(1e4), help='Model s
 parser.add_argument('--eval_interval', type=int, default=int(1e4), help='Model evaluating interval, in steps.')
 parser.add_argument('--eval_turns', type=int, default=3, help='How many episodes for eval')
 parser.add_argument('--random_steps', type=int, default=int(5e4), help='steps for random policy to explore')
-parser.add_argument('--update_every', type=int, default=100, help='training frequency')
+parser.add_argument('--update_every', type=int, default=1000, help='training frequency')
 parser.add_argument('--eps_decay_rate', type=int, default=3000, help='decay rate every n episodes')
 
 parser.add_argument('--gamma', type=float, default=0.99, help='Discounted Factor')
-parser.add_argument('--net_width', type=int, default=256, help='Hidden net width')
+parser.add_argument('--net_width', type=int, default=128, help='Hidden net width')
 parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate')
-parser.add_argument('--batch_size', type=int, default=256, help='lenth of sliced trajectory')
+parser.add_argument('--batch_size', type=int, default=400, help='lenth of sliced trajectory')
 parser.add_argument('--epsilon', type=float, default=1.0, help='eps for e greedy strategy')
-parser.add_argument('--epsilon_decay', type=float, default=0.998, help='decay rate of exploration')
+parser.add_argument('--epsilon_decay', type=float, default=0.999, help='decay rate of exploration')
 parser.add_argument('--epsilon_min', type=float, default=0.1, help='min value for e greedy eps value')
 parser.add_argument('--Double', type=str2bool, default=True, help='Whether to use Double Q-learning')
 parser.add_argument('--Duel', type=str2bool, default=True, help='Whether to use Duel networks')
-
-parser.add_argument('--debugging', type=str2bool, default=False, help='Whether to print values during training for debugging')
 opt = parser.parse_args()
 opt.dvc = torch.device(opt.dvc)
 
 
 EnvName = ['SoulsGymIudexDemo-v0'] # SoulsGymIudexDemo-v0 => for full fight to test out agent
-BriefEnvName = ['Iudex-v1.4-basic-reward'] #
+BriefEnvName = ['Iudex-v2.0-reduced-action-space'] #
 
 if opt.CustomReward:
     # wrapper to add our new parameters to the reward function while still being able to access the game state and next game state
@@ -64,6 +62,20 @@ if opt.CustomReward:
     IudexEnv.compute_reward = staticmethod(reward_wrapper)
 
 demo_env = gym.make(EnvName[opt.EnvIdex])
+new_action_space = {
+        0: ['forward'], 1: ['forward', 'right'], 2: ['right'], 3: ['right', 'backward'], 4: ['backward'], 
+        5: ['backward', 'left'], 6: ['left'], 7: ['left', 'forward'], 8: ['forward', 'roll'],  
+        9: ['right', 'roll'], 10: ['backward', 'roll'],  11: ['left', 'roll'], 
+         12: ['lightattack'], 13: ['heavyattack'], 14: ['parry'], 15: []}
+    
+# REPLACE WITH YOUR OWN VENV PATH THIS IS ONLY TEMPORARY 
+# I KNOW IT LOOKS HORRIBLE BUT I WILL CHANGE IT LATER
+with open(r"D:\GAP YEAR\RL-Souls\DDDQN\dddqnvenv\Lib\site-packages\soulsgym\core\data\darksouls3\actions.yaml", 'w') as file:
+    yaml.dump(new_action_space, file, default_flow_style=False)
+
+#env.action_space = new_action_space
+demo_env.action_space = gym.spaces.Discrete(15)
+
 demo_env = PreprocessedEnvWrapper(demo_env, flatten_observation)
 opt.state_dim = 26 # env.observation_space.shape[0] # PLEASE FIX THIS LATER
 opt.action_dim = demo_env.action_space.n
@@ -89,7 +101,7 @@ print('Algorithm:',algo_name,'  Env:',BriefEnvName[opt.EnvIdex],'  state_dim:',o
 agent = DQN_agent(**vars(opt))
 
 load_algo_name = 'DuelDDQN'
-load_env_name = 'Iudex-v1.0'
+load_env_name = 'Iudex-v2.0-reduced-action-space'
 resumed_steps = agent.load(load_algo_name, load_env_name, checkpoint=True)
 agent.replay_buffer.load(f"./model/{load_algo_name}_{load_env_name}_replaybuf.pth")
 agent.q_net.eval()
